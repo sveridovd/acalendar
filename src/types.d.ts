80d844e5072bf9602f58ad6142c1d501e49e8057
Svieridov Denis <sveridov.denis@yahoo.com>
2020-06-09 15:17:41 +0300

    fix types.d.ts

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,11 +1,10 @@
 import { Component } from "react";
 import { Moment } from "moment";
 
-
 interface CalendarProps {
     date: Moment;
     onChange(date: Moment);
 }
 
-export interface Calendar extends Component<CalendarProps> {}
-export interface CalendarHolder extends Component<CalendarProps> {}
+export declare interface Calendar extends Component<CalendarProps> {}
+export declare interface CalendarHolder extends Component<CalendarProps> {}
